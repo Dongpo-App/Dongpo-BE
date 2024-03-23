75f09ca62269ab42bb5189e6ddd31a480ddc93d8
h0ch1 <h0ch1@naver.com>
2024-03-23 16:50:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/dongyang/dongpo/DongpoApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.dongpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongpoApplication.class, args);
+    }
+
+}
