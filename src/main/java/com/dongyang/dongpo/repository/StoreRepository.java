92b5111abaf379aa8418332196b12ed2605a52cb
Seungwan Yoo <ysw99@kakao.com>
2024-05-21 12:37:20 +0900

    공통 변동사항 반영
    * index 페이지 컨트롤러 추가
    
    * Store 엔티티 수정 및 위도, 경도 DTO 추가
      - Store 엔티티 : latitude, longitude 필드 추가 (location 필드 삭제)
      - 위도, 경도 DTO 추가
      - StoreRepository 추가

---
+++ src/main/java/com/dongyang/dongpo/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.dongyang.dongpo.repository;
+
+import com.dongyang.dongpo.domain.store.Store;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StoreRepository extends CrudRepository<Store, Long> {
+
+}
