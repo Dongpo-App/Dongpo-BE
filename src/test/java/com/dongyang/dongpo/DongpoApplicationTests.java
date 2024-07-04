28d7409858edfabb2414f4f31b70f12e1633f668
Seungwan Yoo <ysw99@kakao.com>
2024-07-04 16:03:11 +0900

    workflow gradle 빌드 과정 수정

--- src/test/java/com/dongyang/dongpo/DongpoApplicationTests.java
+++ src/test/java/com/dongyang/dongpo/DongpoApplicationTests.java
@@ -3,7 +3,7 @@ package com.dongyang.dongpo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DongpoApplicationTests.class)
 class DongpoApplicationTests {
 
     @Test
