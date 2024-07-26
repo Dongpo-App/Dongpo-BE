27fb09d3de29baac974d4a281ad7a6151e2406f5
h0ch1 <h0ch1@naver.com>
2024-07-26 10:26:34 +0900

    방문인증, 점포등록 랭킹 추가

--- src/main/java/com/dongyang/dongpo/dto/rank/RankDto.java
+++ src/main/java/com/dongyang/dongpo/dto/rank/RankDto.java
@@ -1,2 +1,18 @@
-package com.dongyang.dongpo.dto.rank;public class RankDto {
+package com.dongyang.dongpo.dto.rank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RankDto {
+
+    private String nickname;
+    private Long count;
+
+    public static RankDto toDto(Object[] obj) {
+        return new RankDto((String) obj[0], (Long) obj[1]);
+    }
 }
--- src/main/java/com/dongyang/dongpo/service/rank/RankService.java
+++ src/main/java/com/dongyang/dongpo/service/rank/RankService.java
@@ -1,2 +1,48 @@
-package com.dongyang.dongpo.service.rank;public class RankService {
+package com.dongyang.dongpo.service.rank;
+
+import com.dongyang.dongpo.dto.rank.RankDto;
+import com.dongyang.dongpo.repository.store.StoreRepository;
+import com.dongyang.dongpo.repository.store.StoreReviewRepository;
+import com.dongyang.dongpo.repository.store.StoreVisitCertRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class RankService {
+
+
+    private final StoreReviewRepository reviewRepository;
+    private final StoreVisitCertRepository visitCertRepository;
+    private final StoreRepository storeRepository;
+
+    public List<RankDto> getVisitRank() {
+        List<Object[]> visitRank  = visitCertRepository.findTop10MembersBySuccessfulVisitCount();
+        return visitRank.stream()
+                .map(result -> new RankDto((String) result[0], (Long) result[1]))
+                .collect(Collectors.toList());
+    }
+
+
+    public List<RankDto> getReviewRank() {
+        List<Object[]> reviewRanks = reviewRepository.findTop10MembersByReviewCount();
+        return reviewRanks.stream()
+                .map(result -> new RankDto((String) result[0], (Long) result[1]))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<RankDto> getStoreRank() {
+        List<Object[]> storeRank  = storeRepository.findTop10MembersByStoreCount();
+        return storeRank.stream()
+                .map(result -> new RankDto((String) result[0], (Long) result[1]))
+                .collect(Collectors.toList());
+    }
 }
