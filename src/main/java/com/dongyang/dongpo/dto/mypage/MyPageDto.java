d384b1c7bc774b160b4b15fa17f3705883c0e8c7
Seungwan Yoo <ysw99@kakao.com>
2024-08-05 23:51:43 +0900

    마이페이지 응답 issue 수정

--- src/main/java/com/dongyang/dongpo/dto/mypage/MyPageDto.java
+++ src/main/java/com/dongyang/dongpo/dto/mypage/MyPageDto.java
@@ -4,6 +4,9 @@ import com.dongyang.dongpo.domain.member.Member;
 import com.dongyang.dongpo.domain.member.MemberTitle;
 import com.dongyang.dongpo.domain.member.Title;
 import com.dongyang.dongpo.domain.store.Store;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 
 import java.util.List;
@@ -15,12 +18,16 @@ import java.util.List;
 public class MyPageDto {
     private String nickname;
     private String profilePic;
+    @JsonProperty("mainTitle")
     private TitleDto mainTitle; // 사용자 메인 칭호
     private List<TitleDto> titles; // 사용자 칭호 모음
     private int registerCount;
     private int titleCount;
     private int presentCount;
 
+    @Data
+    @JsonSerialize
+    @JsonDeserialize
     @Builder
     public static class TitleDto {
         private Title title;
