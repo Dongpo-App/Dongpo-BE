cb88875642846f02fa911f6d8123b7f470632aaf
Seungwan Yoo <ysw99@kakao.com>
2024-09-17 16:04:22 +0900

    #33 MyPageService 테스트 케이스 작성

--- src/test/java/com/dongyang/dongpo/service/mypage/MyPageServiceTest.java
+++ src/test/java/com/dongyang/dongpo/service/mypage/MyPageServiceTest.java
@@ -3,156 +3,82 @@ package com.dongyang.dongpo.service.mypage;
 import com.dongyang.dongpo.domain.member.Member;
 import com.dongyang.dongpo.domain.member.MemberTitle;
 import com.dongyang.dongpo.domain.member.Title;
-import com.dongyang.dongpo.domain.store.Store;
 import com.dongyang.dongpo.dto.mypage.MyPageDto;
 import com.dongyang.dongpo.dto.mypage.MyPageUpdateDto;
 import com.dongyang.dongpo.repository.member.MemberRepository;
 import com.dongyang.dongpo.repository.member.MemberTitleRepository;
 import com.dongyang.dongpo.repository.store.StoreRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-@Rollback
+@ExtendWith(MockitoExtension.class)
 class MyPageServiceTest {
 
-    @Autowired
+    @Mock
     private MemberRepository memberRepository;
 
-    @Autowired
+    @Mock
     private MemberTitleRepository memberTitleRepository;
 
-    @Autowired
+    @Mock
     private StoreRepository storeRepository;
 
-    @BeforeEach
-    void setUp() {
-        Member member = Member.builder()
-                .email("test@example.com")
-                .name("테스트")
-                .nickname("테스터")
-                .profilePic(null)
-                .mainTitle(Title.BASIC_TITLE)
-                .role(Member.Role.ROLE_MEMBER)
-                .build();
-        memberRepository.save(member);
-
-        MemberTitle memberTitle1 = MemberTitle.builder()
-                .member(member)
-                .title(Title.REGULAR_CUSTOMER)
-                .achieveDate(LocalDateTime.now())
-                .build();
-        MemberTitle memberTitle2 = MemberTitle.builder()
-                .member(member)
-                .title(Title.BASIC_TITLE)
-                .achieveDate(LocalDateTime.now())
-                .build();
-        memberTitleRepository.save(memberTitle1);
-        memberTitleRepository.save(memberTitle2);
-
-        Store store1 = Store.builder()
-                .name("역할맥")
-                .address("서울시 구로구 고척동")
-                .latitude(37.50054492371818)
-                .longitude(126.86630168247805)
-                .openTime(LocalTime.of(18, 0))
-                .closeTime(LocalTime.of(23, 0))
-                .member(member)
-                .isToiletValid(true)
-                .status(Store.StoreStatus.ACTIVE)
-                .build();
-
-        Store store2 = Store.builder()
-                .name("시골집")
-                .address("서울시 구로구 고척동")
-                .latitude(37.50109695453075)
-                .longitude(126.86845508548086)
-                .openTime(LocalTime.of(16, 0))
-                .closeTime(LocalTime.of(21, 0))
-                .member(member)
-                .isToiletValid(false)
-                .status(Store.StoreStatus.ACTIVE)
-                .build();
-
-        Store store3 = Store.builder()
-                .name("써브웨이")
-                .address("서울시 구로구 고척동")
-                .latitude(37.500986658450685)
-                .longitude(126.86651576780534)
-                .openTime(LocalTime.of(16, 0))
-                .closeTime(LocalTime.of(21, 0))
-                .member(member)
-                .isToiletValid(true)
-                .status(Store.StoreStatus.ACTIVE)
-                .build();
-
-        storeRepository.save(store1);
-        storeRepository.save(store2);
-        storeRepository.save(store3);
-    }
+    @InjectMocks
+    private MyPageService myPageService;
 
     @Test
     void getMyPageIndex() {
         // given
-        Member memberByEmail = memberRepository.findByEmail("test@example.com").orElse(null);
+        Member member = mock(Member.class);
+        Title title = mock(Title.class);
+
+        when(member.getMainTitle()).thenReturn(title);
+        when(title.getDescription()).thenReturn("Title Description");
+        when(member.getNickname()).thenReturn("테스터");
 
         // when
-        List<MemberTitle> memberTitles = memberTitleRepository.findByMember(memberByEmail);
-        List<Store> memberStores = storeRepository.findByMember(memberByEmail);
-        MyPageDto myPageDto = MyPageDto.toEntity(memberByEmail, memberTitles, memberStores);
+        MyPageDto myPageIndex = myPageService.getMyPageIndex(member);
 
         // then
-        System.out.println("myPageDto = " + myPageDto.toString());
-        assertThat(memberByEmail).isNotNull();
-        assertEquals(memberByEmail.getEmail(), "test@example.com");
-        assertThat(memberTitles).isNotEmpty();
+        verify(memberTitleRepository).findByMember(member);
+        verify(storeRepository).findByMember(member);
+        assertThat(myPageIndex.getNickname()).isEqualTo(member.getNickname());
     }
 
     @Test
     void updateMyPageInfo() {
         // given
-        Member member = memberRepository.findByEmail("test@example.com").orElse(null);
-        System.out.println("member = " + member.getNickname());
+        Member member = mock(Member.class);
+        MyPageUpdateDto myPageUpdateDto = mock(MyPageUpdateDto.class);
+        MemberTitle memberTitle = mock(MemberTitle.class);
 
-        MyPageUpdateDto myPageUpdateDto = MyPageUpdateDto.builder()
-                .nickname("테스터2")
-                .profilePic("https://example.com/profile.jpg")
-                .newMainTitle(Title.REGULAR_CUSTOMER)
-                .build();
+        when(member.getMainTitle()).thenReturn(Title.BASIC_TITLE);
+        when(member.getNickname()).thenReturn("테스터");
+        when(member.getProfilePic()).thenReturn(null);
+
+        when(myPageUpdateDto.getNickname()).thenReturn("테스터2");
+        when(myPageUpdateDto.getProfilePic()).thenReturn("https://test.com/new-profile.jpg"); // 업데이트 할 프로필 이미지
+        when(myPageUpdateDto.getNewMainTitle()).thenReturn(Title.REGULAR_CUSTOMER); // 업데이트 할 메인 칭호
+        when(memberTitle.getTitle()).thenReturn(Title.REGULAR_CUSTOMER);
+
+        when(memberRepository.findByEmail(any())).thenReturn(Optional.of(member));
+        when(memberTitleRepository.findByMemberAndTitle(any(), any())).thenReturn(memberTitle);
 
         // when
-        if (myPageUpdateDto.getProfilePic() != null && !myPageUpdateDto.getProfilePic().isBlank()) {
-            member.setProfilePic(myPageUpdateDto.getProfilePic());
-        }
-        if (myPageUpdateDto.getNickname() != null && !myPageUpdateDto.getNickname().equals(member.getNickname())) {
-            member.setNickname(myPageUpdateDto.getNickname());
-        }
-        if (myPageUpdateDto.getNewMainTitle() != null && !myPageUpdateDto.getNewMainTitle().equals(member.getMainTitle())) {
-            MemberTitle memberTitle = memberTitleRepository.findByMemberAndTitle(member, myPageUpdateDto.getNewMainTitle());
-            member.setMainTitle(memberTitle.getTitle());
-        }
-
-        Member foundMember = memberRepository.findByEmail("test@example.com").orElse(null);
+        myPageService.updateMyPageInfo(member.getEmail(), myPageUpdateDto);
 
         // then
-        System.out.println("foundMember = " + foundMember.getNickname());
-        assertThat(foundMember.getNickname()).isEqualTo("테스터2");
-        assertThat(foundMember.getProfilePic()).isEqualTo("https://example.com/profile.jpg");
-        assertThat(foundMember.getMainTitle()).isEqualTo(Title.REGULAR_CUSTOMER);
+        verify(member).setProfilePic("https://test.com/new-profile.jpg");
+        verify(member).setNickname("테스터2");
+        verify(member).setMainTitle(Title.REGULAR_CUSTOMER);
     }
 }
