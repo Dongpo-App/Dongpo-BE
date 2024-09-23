957dd173d2279757f9cfe6e783ebdbd997452062
Seungwan Yoo <ysw99@kakao.com>
2024-09-23 13:01:04 +0900

    Repository 추가 - StoreOperatingDayRepository - StorePayMethodRepository

---
+++ src/main/java/com/dongyang/dongpo/repository/store/StoreOperatingDayRepository.java
@@ -0,0 +1,7 @@
+package com.dongyang.dongpo.repository.store;
+
+import com.dongyang.dongpo.domain.store.StoreOperatingDay;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreOperatingDayRepository extends JpaRepository<StoreOperatingDay, Long> {
+}
---
+++ src/main/java/com/dongyang/dongpo/repository/store/StorePayMethodRepository.java
@@ -0,0 +1,7 @@
+package com.dongyang.dongpo.repository.store;
+
+import com.dongyang.dongpo.domain.store.StorePayMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StorePayMethodRepository extends JpaRepository<StorePayMethod, Long> {
+}
