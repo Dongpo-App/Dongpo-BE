61fcfc8106f12651f7245e6968fe03b8108a210b
Seungwan Yoo <ysw99@kakao.com>
2024-09-23 13:02:23 +0900

    점포 등록 로직 수정 - 결제 수단, 오픈 요일 데이터 저장 구현 - 테스트 케이스 재작성

--- src/test/java/com/dongyang/dongpo/service/store/StoreServiceTest.java
+++ src/test/java/com/dongyang/dongpo/service/store/StoreServiceTest.java
@@ -2,8 +2,12 @@ package com.dongyang.dongpo.service.store;
 
 import com.dongyang.dongpo.domain.member.Member;
 import com.dongyang.dongpo.domain.store.Store;
+import com.dongyang.dongpo.domain.store.StoreOperatingDay;
+import com.dongyang.dongpo.domain.store.StorePayMethod;
 import com.dongyang.dongpo.dto.store.StoreDto;
 import com.dongyang.dongpo.dto.store.StoreRegisterDto;
+import com.dongyang.dongpo.repository.store.StoreOperatingDayRepository;
+import com.dongyang.dongpo.repository.store.StorePayMethodRepository;
 import com.dongyang.dongpo.repository.store.StoreRepository;
 import com.dongyang.dongpo.service.location.LocationService;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -24,6 +29,12 @@ class StoreServiceTest {
 
     @Mock
     private StoreRepository storeRepository;
+
+    @Mock
+    private StorePayMethodRepository storePayMethodRepository;
+
+    @Mock
+    private StoreOperatingDayRepository storeOperatingDayRepository;
 
     @InjectMocks
     private StoreService storeService;
@@ -38,15 +49,23 @@ class StoreServiceTest {
         StoreRegisterDto storeDto = mock(StoreRegisterDto.class);
         Member member = mock(Member.class);
         Store store = mock(Store.class);
+        StorePayMethod storePayMethod = mock(StorePayMethod.class);
+        StoreOperatingDay storeOperatingDay = mock(StoreOperatingDay.class);
 
         when(locationService.verifyStoreRegistration(storeDto)).thenReturn(true);
         when(storeRepository.save(any())).thenReturn(store);
+        when(storePayMethodRepository.save(any())).thenReturn(storePayMethod);
+        when(storeOperatingDayRepository.save(any())).thenReturn(storeOperatingDay);
+        when(storeDto.getPayMethods()).thenReturn(List.of(Store.PayMethod.CASH, Store.PayMethod.CARD));
+        when(storeDto.getOperatingDays()).thenReturn(List.of(Store.OperatingDay.MON, Store.OperatingDay.TUE));
 
         // when
         storeService.addStore(storeDto, member);
 
         // then
         verify(storeRepository).save(any());
+        verify(storePayMethodRepository, times(2)).save(any());
+        verify(storeOperatingDayRepository, times(2)).save(any());
     }
 
     @Test
@@ -65,6 +84,17 @@ class StoreServiceTest {
 
     @Test
     void detailStore() {
+        // given
+        Store store = mock(Store.class);
+        Optional<Store> optionalStore = Optional.of(store);
+
+        when(storeRepository.findById(any())).thenReturn(optionalStore);
+
+        // when
+        storeService.detailStore(store.getId());
+
+        // then
+        verify(storeRepository).findById(any());
     }
 
     @Test
