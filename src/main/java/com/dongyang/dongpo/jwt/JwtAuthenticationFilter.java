0ceb34be9ec1c883d92353a67242880bfe977c48
h0ch1 <h0ch1@naver.com>
2024-09-23 10:41:20 +0900

    #39 jwt 예외 롤백

--- src/main/java/com/dongyang/dongpo/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/dongyang/dongpo/jwt/JwtAuthenticationFilter.java
@@ -1,42 +1,46 @@
 package com.dongyang.dongpo.jwt;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
+import com.dongyang.dongpo.exception.CustomException;
+import com.dongyang.dongpo.exception.ErrorCode;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends GenericFilterBean {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // Authorization 헤더를 찾아 검증한다
-        String authorization = request.getHeader("Authorization");
-
-        // 인증 정보가 아예 없을 경우 바로 다음 필터로 넘어간다
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        // accessToken 파싱
-        String accessToken = jwtTokenProvider.parseAccessToken(authorization);
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         try {
-            if (jwtTokenProvider.validateToken(accessToken))
-                SecurityContextHolder.getContext().setAuthentication(jwtTokenProvider.getAuthentication(accessToken));
-        } catch (Exception e) {
-            log.error("Error while parsing JWT Token: ", e);
-            throw new RuntimeException(e);
+            String token = resolveToken((HttpServletRequest) servletRequest);
+
+            if (jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch (Exception e){
+            servletRequest.setAttribute("exception", e);
+        }finally {
+            filterChain.doFilter(servletRequest, servletResponse);
         }
-        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest servletRequest) throws Exception{
+        String token = servletRequest.getHeader("Authorization");
+        if (token == null || !token.startsWith("Bearer "))
+            throw new CustomException(ErrorCode.CLAIMS_NOT_FOUND);
+
+        return token.substring(7);
     }
 }
