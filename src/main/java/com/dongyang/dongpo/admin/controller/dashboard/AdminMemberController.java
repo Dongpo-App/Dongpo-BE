ee26c2a52afe1a9d0934cfa9c06f2ac27d2b3313
h0ch1 <h0ch1@naver.com>
2024-09-25 13:18:39 +0900

    중간 커밋

--- src/main/java/com/dongyang/dongpo/admin/controller/dashboard/AdminMemberController.java
+++ src/main/java/com/dongyang/dongpo/admin/controller/dashboard/AdminMemberController.java
@@ -3,6 +3,7 @@ package com.dongyang.dongpo.admin.controller.dashboard;
 import com.dongyang.dongpo.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +16,10 @@ public class AdminMemberController {
     private final MemberService memberService;
 
     @GetMapping("/{id}")
-    public String detail(@PathVariable("id") Long id) {
-        memberService.findOne(id);
-        return "null";
+    public String detail(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("member", memberService.findOne(id));
+        return "admin/dashboard/member/member_detail";
     }
-
 
 
 }
--- src/main/java/com/dongyang/dongpo/dto/admin/member/MemberDto.java
+++ src/main/java/com/dongyang/dongpo/dto/admin/member/MemberDto.java
@@ -1,2 +1,34 @@
-package com.dongyang.dongpo.dto.admin.member;public class MemberDto {
+package com.dongyang.dongpo.dto.admin.member;
+
+import com.dongyang.dongpo.domain.member.Member;
+import com.dongyang.dongpo.domain.member.MemberTitle;
+import com.dongyang.dongpo.domain.member.Title;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class MemberDto {
+    private Long id;
+    private String email;
+    private String name;
+    private String nickname;
+    private String profilePic;
+    private Title mainTitle;
+    private List<MemberTitle> memberTitles;
+    private Member.Role role;
+    private Member.Gender gender;
+    private String ageGroup;
+    private Member.SocialType socialType;
+    private String socialId;
+    private LocalDateTime signupDate;
+    private LocalDateTime leaveDate;
+    private Member.Status status;
+
 }
