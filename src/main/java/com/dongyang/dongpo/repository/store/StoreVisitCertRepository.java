aaf9e0533bc4a84fa09e1764219b6da28784d394
h0ch1 <h0ch1@naver.com>
2024-09-25 15:20:11 +0900

    로직 수정 (오류수정)

--- src/main/java/com/dongyang/dongpo/repository/store/StoreVisitCertRepository.java
+++ src/main/java/com/dongyang/dongpo/repository/store/StoreVisitCertRepository.java
@@ -1,16 +1,13 @@
 package com.dongyang.dongpo.repository.store;
 
 import com.dongyang.dongpo.domain.member.Member;
-import com.dongyang.dongpo.domain.store.OpenTime;
 import com.dongyang.dongpo.domain.store.Store;
 import com.dongyang.dongpo.domain.store.StoreVisitCert;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.DayOfWeek;
 import java.util.List;
-import java.util.Optional;
 
 public interface StoreVisitCertRepository extends JpaRepository<StoreVisitCert, Long> {
 
@@ -21,10 +18,7 @@ public interface StoreVisitCertRepository extends JpaRepository<StoreVisitCert, 
             "ORDER BY visitCount DESC")
     List<Object[]> findTop10MembersBySuccessfulVisitCount(Pageable pageable);
 
-    Optional<StoreVisitCert> findByStore(Store store);
-
-    // 해당 요일, 시간에 방문 인증 성공한 데이터가 존재하는지 확인
-    boolean existsByOpenDayAndOpenTimeAndIsVisitSuccessfulTrue(DayOfWeek dayOfWeek, OpenTime openTime);
+    List<StoreVisitCert> findByStoreAndAndIsVisitSuccessfulTrue(Store store);
 
     Long countByStoreAndIsVisitSuccessfulTrue(Store store); // 점포 방문인증 성공 카운트
     Long countByMemberAndIsVisitSuccessfulIsTrue(Member member); // 방문 인증 성공 카운트
--- src/main/java/com/dongyang/dongpo/service/open/OpenPossibilityService.java
+++ src/main/java/com/dongyang/dongpo/service/open/OpenPossibilityService.java
@@ -1,11 +1,15 @@
 package com.dongyang.dongpo.service.open;
 
+import com.dongyang.dongpo.domain.store.OpenTime;
 import com.dongyang.dongpo.domain.store.Store;
-import com.dongyang.dongpo.domain.store.StoreVisitCert;
 import com.dongyang.dongpo.dto.store.OpenPossibility;
 import com.dongyang.dongpo.repository.store.StoreVisitCertRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,17 +18,45 @@ public class OpenPossibilityService {
     private final StoreVisitCertRepository storeVisitCertRepository;
 
     public OpenPossibility getOpenPossibility(Store store) {
-        return storeVisitCertRepository.findByStore(store)
-                .map(storeVisitCert -> {
-                    // 점포의 영업 시간과 요일을 확인하여 영업 가능 여부 판단
-                    if (storeVisitCertRepository.existsByOpenDayAndOpenTimeAndIsVisitSuccessfulTrue(
-                            storeVisitCert.getOpenDay(), storeVisitCert.getOpenTime())) {
-                        return OpenPossibility.HIGH;
-                    } else {
-                        return OpenPossibility.NONE; // 성공 데이터 없는 경우
-                    }
+        LocalDateTime now = LocalDateTime.now();
+        DayOfWeek dayOfWeek = now.getDayOfWeek(); // 현재 요일
+        OpenTime openTime = getOpenTime(now); // 현재 시간대
+
+        // 해당 점포 방문인증 성공한 데이터만 조회
+        List<OpenPossibility> possibilities = storeVisitCertRepository.findByStoreAndAndIsVisitSuccessfulTrue(store)
+                .stream()
+                .map(s -> {
+                    if (s.getOpenTime().equals(openTime) && s.getOpenDay().equals(dayOfWeek))
+                        return OpenPossibility.HIGH; // 성공한 경우
+                    else
+                        return OpenPossibility.NONE; // 실패한 경우
                 })
-                .orElse(OpenPossibility.NONE); // 방문 인증 데이터가 없는 경우
+                .toList(); // 결과를 리스트로 수집
+
+        if (possibilities.contains(OpenPossibility.HIGH))
+            return OpenPossibility.HIGH; // 하나라도 HIGH가 있으면 HIGH 반환
+        else
+            return OpenPossibility.NONE; // 모두 NONE이면 NONE 반환
+
     }
 
+    private OpenTime getOpenTime(LocalDateTime now){
+        int hour = now.getHour();
+        if (hour < 3)
+            return OpenTime.MIDNIGHT_TO_3AM;
+        else if (hour < 6)
+            return OpenTime.THREE_AM_TO_6AM;
+        else if (hour < 9)
+            return OpenTime.SIX_AM_TO_9AM;
+        else if (hour < 12)
+            return OpenTime.NINE_AM_TO_NOON;
+        else if (hour < 15)
+            return OpenTime.NOON_TO_3PM;
+        else if (hour < 18)
+            return OpenTime.THREE_PM_TO_6PM;
+        else if (hour < 21)
+            return OpenTime.SIX_PM_TO_9PM;
+        else
+            return OpenTime.NINE_PM_TO_MIDNIGHT;
+    }
 }
