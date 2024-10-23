dd34867c52a2bf1f6c8a26f5c2688c6c65227c38
JunYoung Choi <h0ch1@naver.com>
2024-10-23 23:00:32 +0900

    reissue 수정 (#112)
    * reissue 수정
    
    * 테스트 수정

--- src/main/java/com/dongyang/dongpo/repository/RefreshTokenRepository.java
+++ src/main/java/com/dongyang/dongpo/repository/RefreshTokenRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByEmail(String email);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
