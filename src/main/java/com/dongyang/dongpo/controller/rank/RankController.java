91d7843494a7810c9b3c969169a01068ef2d748b
JunYoung Choi <h0ch1@naver.com>
2024-11-06 14:01:05 +0900

    랭킹 엔드 포인트 수정 - #116 (#122)

--- src/main/java/com/dongyang/dongpo/controller/rank/RankController.java
+++ src/main/java/com/dongyang/dongpo/controller/rank/RankController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/rank")
+@RequestMapping("/api/rank")
 @RequiredArgsConstructor
 public class RankController {
 
