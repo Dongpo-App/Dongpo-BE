fe98ea1a8751d92ea896b38a23f3a7695c4b9bbc
Seungwan Yoo <ysw99@kakao.com>
2024-11-12 10:09:15 +0900

    refactor: 북마크 추가/삭제 응답 값 수정 - 추가/삭제 시 해당 점포의 북마크 갯수와 로그인 한 사용자의 북마크 등록 여부 응답

--- src/main/java/com/dongyang/dongpo/controller/bookmark/BookmarkController.java
+++ src/main/java/com/dongyang/dongpo/controller/bookmark/BookmarkController.java
@@ -2,6 +2,7 @@ package com.dongyang.dongpo.controller.bookmark;
 
 import com.dongyang.dongpo.domain.member.Member;
 import com.dongyang.dongpo.dto.apiresponse.ApiResponse;
+import com.dongyang.dongpo.dto.bookmark.BookmarkResponseDto;
 import com.dongyang.dongpo.service.bookmark.BookmarkService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +19,15 @@ public class BookmarkController {
 
     @PostMapping("/{storeId}")
     @Operation(summary = "북마크 추가")
-    public ResponseEntity<ApiResponse<String>> addBookmark(@PathVariable Long storeId,
-                                                           @AuthenticationPrincipal Member member) {
-        bookmarkService.addBookmark(member, storeId);
-        return ResponseEntity.ok(new ApiResponse<>("success"));
+    public ResponseEntity<ApiResponse<BookmarkResponseDto>> addBookmark(@PathVariable final Long storeId,
+                                                                        @AuthenticationPrincipal final Member member) {
+        return ResponseEntity.ok(new ApiResponse<>(bookmarkService.addBookmark(member, storeId)));
     }
 
     @DeleteMapping("/{storeId}")
     @Operation(summary = "북마크 제거")
-    public ResponseEntity<ApiResponse<String>> deleteBookmark(@PathVariable Long storeId,
-                                                              @AuthenticationPrincipal Member member) {
-        bookmarkService.deleteBookmark(storeId, member);
-        return ResponseEntity.ok(new ApiResponse<>("success"));
+    public ResponseEntity<ApiResponse<BookmarkResponseDto>> deleteBookmark(@PathVariable final Long storeId,
+                                                              @AuthenticationPrincipal final Member member) {
+        return ResponseEntity.ok(new ApiResponse<>(bookmarkService.deleteBookmark(storeId, member)));
     }
 }
