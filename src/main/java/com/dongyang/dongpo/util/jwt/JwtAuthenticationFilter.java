cce59f7302f76589828ea9852f45b9febed87e7e
Seungwan Yoo <ysw99@kakao.com>
2024-12-27 03:28:43 +0900

    refactor: Jwt 필터 개선 - 상속 Filter 변경 (OncePerRequestFilter) - 토큰 검증, 파싱 로직 분리 및 개선 - 상수 필드 추가

--- src/main/java/com/dongyang/dongpo/util/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/dongyang/dongpo/util/jwt/JwtAuthenticationFilter.java
@@ -2,48 +2,63 @@ package com.dongyang.dongpo.util.jwt;
 
 import com.dongyang.dongpo.exception.CustomException;
 import com.dongyang.dongpo.exception.ErrorCode;
+import io.netty.util.internal.StringUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtTokenProvider jwtTokenProvider;
+    private final String HEADER_PREFIX = "Authorization";
+    private final String TOKEN_PREFIX = "Bearer ";
+
+    private final JwtUtil jwtUtil;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
-            String token = resolveToken((HttpServletRequest) servletRequest);
-
-            jwtTokenProvider.isBlacklisted(token);
-
-            if (jwtTokenProvider.validateToken(token)) {
-                Authentication authentication = jwtTokenProvider.getAuthentication(token);
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        }catch (Exception e){
-            servletRequest.setAttribute("exception", e);
-        }finally {
-            filterChain.doFilter(servletRequest, servletResponse);
+            String token = resolveToken(request.getHeader(HEADER_PREFIX));
+            validateToken(token);
+            setAuthentication(getAuthentication(token));
+        } catch (Exception e) {
+            request.setAttribute("exception", e);
+        } finally {
+            filterChain.doFilter(request, response);
         }
     }
 
-    private String resolveToken(HttpServletRequest servletRequest) throws Exception{
-        String token = servletRequest.getHeader("Authorization");
-        if (token == null || !token.startsWith("Bearer "))
+    // 토큰 유효성 검증 및 파싱
+    private String resolveToken(String authorizationHeader) {
+        if (StringUtil.isNullOrEmpty(authorizationHeader) || !authorizationHeader.startsWith(TOKEN_PREFIX))
             throw new CustomException(ErrorCode.CLAIMS_NOT_FOUND);
 
-        return token.substring(7);
+        return jwtUtil.parseAccessToken(authorizationHeader);
+    }
+
+    // 토큰 유효성 검증
+    private void validateToken(String token) {
+        jwtUtil.validateToken(token);
+    }
+
+    // 토큰으로 사용자 인증 객체 호출
+    private Authentication getAuthentication(String token) {
+        return jwtUtil.getAuthentication(token);
+    }
+
+    // SecurityContextHolder 사용자 인증 객체 저장
+    private void setAuthentication(Authentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
