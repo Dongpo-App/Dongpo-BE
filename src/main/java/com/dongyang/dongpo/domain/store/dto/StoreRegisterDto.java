1f3b33c8b03a3d5e60d53e8612e3845d307b6236
Seungwan Yoo <ysw99@kakao.com>
2025-01-17 10:54:54 +0900

    feat: 요청 DTO validation 도입

--- src/main/java/com/dongyang/dongpo/domain/store/dto/StoreRegisterDto.java
+++ src/main/java/com/dongyang/dongpo/domain/store/dto/StoreRegisterDto.java
@@ -2,46 +2,86 @@ package com.dongyang.dongpo.domain.store.dto;
 
 import com.dongyang.dongpo.domain.member.entity.Member;
 import com.dongyang.dongpo.domain.store.entity.Store;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalTime;
 import java.util.List;
 
-@Data
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "점포 등록 요청 DTO")
 public class StoreRegisterDto {
 
+    @NotBlank
+    @Schema(description = "점포명", example = "동포식당")
     private String name;
+
+    @NotBlank
+    @Schema(description = "주소", example = "서울특별시 강남구 역삼동")
     private String address;
-    private double latitude;
-    private double longitude;
+
+    @NotNull
+    @DecimalMin(value = "-90.0", message = "Latitude must be greater than or equal to -90.0")
+    @DecimalMax(value = "90.0", message = "Latitude must be less than or equal to 90.0")
+    @Schema(description = "위도", example = "37.123456")
+    private Double latitude;
+
+    @NotNull
+    @DecimalMin(value = "-180.0", message = "Longitude must be greater than or equal to -180.0")
+    @DecimalMax(value = "180.0", message = "Longitude must be less than or equal to 180.0")
+    @Schema(description = "경도", example = "127.123456")
+    private Double longitude;
+
+    @NotNull
+    @Schema(description = "오픈 시간", example = "09:00")
     private LocalTime openTime;
+
+    @NotNull
+    @Schema(description = "마감 시간", example = "21:00")
     private LocalTime closeTime;
+
+    @NotNull
+    @Schema(description = "화장실 여부", example = "true")
     private Boolean isToiletValid;
+
+    @NotEmpty
+    @Valid
+    @Schema(description = "오픈 요일", example = "[\"MON\", \"TUE\"]")
     private List<Store.OperatingDay> operatingDays;
+
+    @NotEmpty
+    @Valid
+    @Schema(description = "결제 수단", example = "[\"CASH\", \"CARD\"]")
     private List<Store.PayMethod> payMethods;
 
-    // 등록하는 사용자의 현재 좌표
-    private double currentLatitude;
-    private double currentLongitude;
+    @NotNull
+    @DecimalMin(value = "-90.0", message = "Current latitude must be greater than or equal to -90.0")
+    @DecimalMax(value = "90.0", message = "Current latitude must be less than or equal to 90.0")
+    @Schema(description = "사용자의 현재 위도", example = "37.123456")
+    private Double currentLatitude;
 
-    public Store toStore(Member member){
+    @NotNull
+    @DecimalMin(value = "-180.0", message = "Current longitude must be greater than or equal to -180.0")
+    @DecimalMax(value = "180.0", message = "Current longitude must be less than or equal to 180.0")
+    @Schema(description = "사용자의 현재 경도", example = "127.123456")
+    private Double currentLongitude;
+
+
+    public Store toEntity(Member member) {
         return Store.builder()
-                .name(name)
-                .address(address)
-                .latitude(latitude)
-                .longitude(longitude)
-                .openTime(openTime)
-                .closeTime(closeTime)
+                .name(this.name)
+                .address(this.address)
+                .latitude(this.latitude)
+                .longitude(this.longitude)
+                .openTime(this.openTime)
+                .closeTime(this.closeTime)
                 .member(member)
-                .isToiletValid(isToiletValid)
+                .isToiletValid(this.isToiletValid)
                 .build();
     }
 }
--- src/main/java/com/dongyang/dongpo/domain/store/dto/StoreUpdateDto.java
+++ src/main/java/com/dongyang/dongpo/domain/store/dto/StoreUpdateDto.java
@@ -1,26 +1,51 @@
 package com.dongyang.dongpo.domain.store.dto;
 
 import com.dongyang.dongpo.domain.store.entity.Store;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.time.LocalTime;
 import java.util.List;
 
-@Data
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "점포 정보 수정 요청 DTO")
 public class StoreUpdateDto {
+
+    @NotBlank
+    @Schema(description = "점포명", example = "동포식당")
     private String name;
+
+    @NotNull
+    @Schema(description = "오픈 시간", example = "09:00")
     private LocalTime openTime;
+
+    @NotNull
+    @Schema(description = "마감 시간", example = "21:00")
     private LocalTime closeTime;
+
+    @NotNull
+    @Schema(description = "화장실 여부", example = "true")
     private Boolean isToiletValid;
+
+    @NotNull
+    @Schema(description = "점포 영업 상태", example = "OPEN")
     private Store.StoreStatus status;
+
+    @NotEmpty
+    @Schema(description = "오픈 요일", example = "[\"MON\", \"TUE\"]")
+    @Valid
     private List<Store.OperatingDay> operatingDays;
+
+    @NotEmpty
+    @Schema(description = "결제 수단", example = "[\"CASH\", \"CARD\"]")
+    @Valid
     private List<Store.PayMethod> payMethods;
+
 }
