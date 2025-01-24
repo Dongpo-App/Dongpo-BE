75e3760af130cab24aa1f69cd85113e7eaa16870
Seungwan Yoo <ysw99@kakao.com>
2025-01-24 23:41:39 +0900

    refactor: StoreReviewController validation 도입, 응답 타입 수정

--- src/main/java/com/dongyang/dongpo/domain/store/controller/StoreReviewController.java
+++ src/main/java/com/dongyang/dongpo/domain/store/controller/StoreReviewController.java
@@ -2,11 +2,13 @@ package com.dongyang.dongpo.domain.store.controller;
 
 import com.dongyang.dongpo.common.dto.apiresponse.ApiResponse;
 import com.dongyang.dongpo.domain.member.entity.Member;
-import com.dongyang.dongpo.domain.store.dto.ReviewDto;
+import com.dongyang.dongpo.domain.store.dto.ReviewRegisterRequestDto;
 import com.dongyang.dongpo.domain.store.dto.StoreReviewResponseDto;
 import com.dongyang.dongpo.domain.store.service.StoreReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,31 +26,31 @@ public class StoreReviewController {
 
     @PostMapping
     @Operation(summary = "리뷰 등록")
-    public ResponseEntity<ApiResponse<String>> addReview(@AuthenticationPrincipal Member member,
-                                                         @PathVariable Long storeId,
-                                                         @RequestBody ReviewDto reviewDto) {
+    public ResponseEntity<ApiResponse<String>> addReview(@AuthenticationPrincipal final Member member,
+                                                         @PathVariable @Min(1) final Long storeId,
+                                                         @Valid @RequestBody final ReviewRegisterRequestDto reviewDto) {
         reviewService.addReview(member, storeId, reviewDto);
-        return ResponseEntity.ok(new ApiResponse<>("success"));
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
     @Operation(summary = "점포 리뷰 전체 조회")
-    public ResponseEntity<ApiResponse<List<StoreReviewResponseDto>>> getReviewByStore(@PathVariable final Long storeId) {
+    public ResponseEntity<ApiResponse<List<StoreReviewResponseDto>>> getReviewByStore(@PathVariable @Min(1) final Long storeId) {
         return ResponseEntity.ok(new ApiResponse<>(reviewService.getReviewsByStore(storeId)));
     }
 
     @GetMapping("/latest")
     @Operation(summary = "점포 리뷰 가장 최신 3건 조회")
-    public ResponseEntity<ApiResponse<List<StoreReviewResponseDto>>> getLatestReviewsByStoreId(@PathVariable final Long storeId) {
+    public ResponseEntity<ApiResponse<List<StoreReviewResponseDto>>> getLatestReviewsByStoreId(@PathVariable @Min(1) final Long storeId) {
         return ResponseEntity.ok(new ApiResponse<>(reviewService.getLatestReviewsByStoreId(storeId)));
     }
 
     @DeleteMapping("/{reviewId}")
     @Operation(summary = "리뷰 삭제")
-    public ResponseEntity<ApiResponse<String>> deleteReview(@PathVariable("storeId") Long storeId,
-                                                            @PathVariable("reviewId") Long reviewId,
-                                                            @AuthenticationPrincipal Member member) {
+    public ResponseEntity<ApiResponse<String>> deleteReview(@PathVariable("storeId") @Min(1) final Long storeId,
+                                                            @PathVariable("reviewId") @Min(1) final Long reviewId,
+                                                            @AuthenticationPrincipal final Member member) {
         reviewService.deleteReview(storeId, reviewId, member);
-        return ResponseEntity.ok(new ApiResponse<>("success"));
+        return ResponseEntity.ok().build();
     }
 }
