920d5d0e32d34159245de31855f3c58cbbccb89d
Seungwan Yoo <ysw99@kakao.com>
2025-02-02 11:20:04 +0900

    refactor: DTO 필드 validation 도입

--- src/main/java/com/dongyang/dongpo/domain/bookmark/dto/BookmarkResponseDto.java
+++ src/main/java/com/dongyang/dongpo/domain/bookmark/dto/BookmarkResponseDto.java
@@ -1,5 +1,7 @@
 package com.dongyang.dongpo.domain.bookmark.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +11,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "북마크 추가/삭제 응답 DTO")
 public class BookmarkResponseDto {
+
+    @Schema(description = "점포 북마크 수", example = "1")
+    @NotNull
     private Long bookmarkCount;
+
+    @Schema(description = "사용자의 북마크 여부", example = "true")
+    @NotNull
     private Boolean isMemberBookmarked;
+
 }
