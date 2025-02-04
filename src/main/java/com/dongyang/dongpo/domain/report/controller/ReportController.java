febac5885ba8a1d505abb31ef41a0f003a4e644f
Seungwan Yoo <ysw99@kakao.com>
2025-02-04 23:41:33 +0900

    refactor: Report 컨트롤러 리팩토링 - 엔드포인트 변경 - Review, Store 별로 나눠져있던 신고 처리, 조회 엔드포인트 병합

--- src/main/java/com/dongyang/dongpo/domain/report/controller/ReportController.java
+++ src/main/java/com/dongyang/dongpo/domain/report/controller/ReportController.java
@@ -2,50 +2,40 @@ package com.dongyang.dongpo.domain.report.controller;
 
 import com.dongyang.dongpo.common.dto.apiresponse.ApiResponse;
 import com.dongyang.dongpo.domain.member.entity.Member;
-import com.dongyang.dongpo.domain.report.dto.ReportDto;
+import com.dongyang.dongpo.domain.report.dto.ReportResponseDto;
+import com.dongyang.dongpo.domain.report.dto.ReportRequestDto;
+import com.dongyang.dongpo.domain.report.enums.ReportType;
 import com.dongyang.dongpo.domain.report.service.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/report")
+@RequestMapping("/api/reports")
 @RequiredArgsConstructor
 public class ReportController {
 
     private final ReportService reportService;
 
-    @PostMapping("/store/{storeId}")
-    @Operation(summary = "점포 신고")
-    public ResponseEntity<ApiResponse<String>> storeReport(@PathVariable("storeId") Long storeId,
-                                                           @AuthenticationPrincipal Member member,
-                                                           @RequestBody ReportDto request) {
-        reportService.addStoreReport(storeId, member, request);
-        return ResponseEntity.ok(new ApiResponse<>("success"));
-    }
-    
-    @PostMapping("/review/{reviewId}")
-    @Operation(summary = "리뷰 신고")
-    public ResponseEntity<ApiResponse<String>> reviewReport(@PathVariable("reviewId") Long reviewId,
-                                                            @AuthenticationPrincipal Member member,
-                                                            @RequestBody ReportDto request) {
-        reportService.addReviewReport(reviewId, member, request);
-        return ResponseEntity.ok(new ApiResponse<>("success"));
+    @PostMapping
+    @Operation(summary = "신고 처리")
+    public ResponseEntity<Void> addReport(@Valid @RequestBody final ReportRequestDto request,
+                                          @AuthenticationPrincipal final Member member) {
+        reportService.addReport(request, member);
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/store/me")
-    @Operation(summary = "내가 신고한 점포조회")
-    public ResponseEntity<ApiResponse<List<ReportDto>>> meReportStore(@AuthenticationPrincipal Member member) {
-        return ResponseEntity.ok(new ApiResponse<>(reportService.myRegStoreReport(member)));
+    @GetMapping("/me")
+    @Operation(summary = "나의 신고 내역 조회")
+    public ResponseEntity<ApiResponse<Page<ReportResponseDto>>> myReports(@RequestParam("type") final ReportType type,
+                                                                          @RequestParam("page") @Min(0) final int page,
+                                                                          @AuthenticationPrincipal final Member member) {
+        return ResponseEntity.ok(new ApiResponse<>(reportService.getMyReports(member, type, page)));
     }
 
-    @GetMapping("/review/me")
-    @Operation(summary = "내가 신고한 리뷰조회")
-    public ResponseEntity<ApiResponse<List<ReportDto>>> meReportReview(@AuthenticationPrincipal Member member) {
-        return ResponseEntity.ok(new ApiResponse<>(reportService.myRegReviewReport(member)));
-    }
 }
